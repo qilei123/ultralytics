852dea95744c5238fe6edde38347c13eaf82889f
qilei123 <qileimail123@gmail.com>
2023-11-14 23:09:34 -0500

    train thyroid x

--- train.sh
+++ train.sh
@@ -10,9 +10,9 @@
 #     mixup=0.5 batch=64 single_cls=True \
 #     save_dir=data_thyroid_yl/ultralytics_run/seg/v8s_mixup05_single device=1
 
-yolo train model=cfgs_thyroid/yolov8m-seg.yaml data=cfgs_thyroid/thyroid-seg.yaml \
-    mixup=0.5 batch=64 single_cls=True \
-    save_dir=data_thyroid_yl/ultralytics_run/seg/v8m_mixup05_single device=0,1
+# yolo train model=cfgs_thyroid/yolov8m-seg.yaml data=cfgs_thyroid/thyroid-seg.yaml \
+#     mixup=0.5 batch=64 single_cls=True \
+#     save_dir=data_thyroid_yl/ultralytics_run/seg/v8m_mixup05_single device=0,1
 
 yolo train model=cfgs_thyroid/yolov8x-seg.yaml data=cfgs_thyroid/thyroid-seg.yaml \
     mixup=0.5 batch=64 single_cls=True \
